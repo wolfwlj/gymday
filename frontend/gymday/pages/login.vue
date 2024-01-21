1e38367580867d23acbdd7c27f4ffd286f7c3768
mevfiew <matthew@gses-system.com>
2024-01-22 00:12:31 +0100

    chore: show password

--- frontend/gymday/pages/login.vue
+++ frontend/gymday/pages/login.vue
@@ -36,7 +36,12 @@ async function login(form){
         <div class="flex flex-col w-[30%] mt-10">
             <!-- inputs -->
             <input v-model="form.Username" type="text" placeholder="Username" class="border border-gray-300 p-2 mb-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
-            <input v-model="form.Password" type="Password" placeholder="Password" class="border border-gray-300 p-2 mb-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
+            <div class="flex mb-4 relative">
+                <input v-model="form.Password" :type="[togglePassword ? 'text' : 'password']" placeholder="Password"
+                    class="border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent flex-1">
+                <UIcon :name="[togglePassword ? 'i-heroicons-eye' : 'i-heroicons-eye-slash']" @click="togglePassword = !togglePassword" class="hover:cursor-pointer absolute right-0 top-3 right-3" />
+            </div>
+
             <!-- button -->
             <button v-on:click="login(form)" type="submit" class="bg-gray-800 text-white py-2 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-opacity-50">Login</button>
         </div>
